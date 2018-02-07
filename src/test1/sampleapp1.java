79b72fc987ff17f35829377a1795d589f2a81241
Davidson Devasigamony <davidson.devasigamony@emc.com>
2018-02-06 22:31:20 -0500

    testing

---
+++ src/test1/sampleapp1.java
@@ -0,0 +1,10 @@
+package test1;
+
+import java.util.Scanner;
+
+public class sampleapp1 {
+	public static void main (String args[]) {
+		System.out.println ("Welcome to Java Programming!!!");
+		
+	}
+}
